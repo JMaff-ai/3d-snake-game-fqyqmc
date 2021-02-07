612aacd45a44bd186b0e322b755cf325f9908473
JMaff-ai <jmaffettone8802@gmail.com>
2021-02-07 03:30:26 -0500

    1 file modified

--- Snake.ts
+++ Snake.ts
@@ -4,20 +4,33 @@ import display from "./display";
 
 class Snake {
     private currentPosition: number;
-    private direction: number;
-    constructor(currentDirection:number) {
-      this.currentPosition = 0;
-      this.direction = currentDirection;
-      let move = this.currentPosition ;
-      if (move < 0) { move = -1}
-      else {move = 1}
-      let turn = currentDirection;
-      if (turn = 0) {turn = -1}
-      else {turn = 1}
+    private currentDirection: number;
+    private xcoord: number;
+    private ycoord: number;
+    
+    public constructor() {
+      this.currentPosition == 0;
+      this.currentDirection == 1;
+      this.xcoord == 0;
+      this.ycoord == 0;
+
     }
-  public get position() {
-    return this.currentPosition;
-  }
-}
-display(Snake)
-export default Snake;
+    public turn() {
+      this.currentDirection *= -1;
+    }
+    public move(squares: number) {
+      this.currentPosition += this.currentDirection * squares;
+    }
+    public newPoint() {
+      this.xcoord += 0;
+      this.ycoord += 0;
+    }
+    public turnLeft() {
+      if
+    }
+    public get position() {
+      return this.currentPosition;
+    
+    }
+    }
+export: any default Snake;
